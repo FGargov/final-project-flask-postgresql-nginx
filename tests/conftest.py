072ec324c6509c6d0e3c547e445073a5bf739ec7
FGargov <fgargov@gmail.com>
2025-05-31 19:05:08 +0300

    Edit database env for CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,13 +7,13 @@ from app.models import db
 
 
 # sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../app")))
-os.environ["DATABASE_URL"] = "sqlite:///:memory:"
+# os.environ["DATABASE_URL"] = "sqlite:///:memory:"
 
 
 @pytest.fixture
 def client():
     flask_app.config["TESTING"] = True
-    flask_app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 
     with flask_app.test_client() as client:
         with flask_app.app_context():
