b8444c80bf35b2892c4ff2eed45cf91fb73a5012
FGargov <fgargov@gmail.com>
2026-03-27 09:06:55 +0200

    fix(tests): align db import path in pytest fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ import os
 
 
 from app.app import app as flask_app
-from app.models import db
+from models import db
 
 
 # sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../app")))
